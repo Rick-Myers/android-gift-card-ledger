e1885314b71d211fe3d9dad8c4d8ce8012bb0a6c
Rick Myers <1986092+Rick-Myers@users.noreply.github.com>
2019-04-12 01:01:21 -0400

    Formatted code and added java docs.

--- app/src/main/java/com/rickmyers/giftcardledger/GiftCardLedger.java
+++ app/src/main/java/com/rickmyers/giftcardledger/GiftCardLedger.java
@@ -6,23 +6,34 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 import com.rickmyers.giftcardledger.database.GiftCardCursorWrapper;
 import com.rickmyers.giftcardledger.database.GiftCardDbSchema.GiftCardTable;
 import com.rickmyers.giftcardledger.database.GiftCardDBHelper;
 
+/**
+ * A gift card ledger. This singleton holds the current state of the ledger and provides persistence.
+ *
+ * @author Rick Myers
+ */
 public class GiftCardLedger {
+
+    // Only one GiftCardLedger may exist at any given time!
     private static GiftCardLedger sGiftCardLedger;
-    private List<GiftCard> mGiftCardList;
-    private Map<UUID, GiftCard> mGiftCardHashMap;
+
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
 
+    /**
+     * Returns a new {@link GiftCardLedger} object if one doesn't exist. Otherwise, returns the
+     * existing GiftCardLedger.
+     *
+     * @param context Context of the hosting activity
+     * @return the new/existing {@link GiftCardLedger}
+     */
     public static GiftCardLedger get(Context context) {
         if (sGiftCardLedger == null) {
             sGiftCardLedger = new GiftCardLedger(context);
@@ -30,35 +41,41 @@ public class GiftCardLedger {
         return sGiftCardLedger;
     }
 
+    /**
+     * Private class constructor
+     *
+     * @param context Context of the hosting activity
+     */
     private GiftCardLedger(Context context) {
-        //mGiftCardHashMap = new LinkedHashMap<>();
         mContext = context.getApplicationContext();
         mDatabase = new GiftCardDBHelper(mContext).getWritableDatabase();
-        /*for (int i = 0; i < 10; i++){
-            GiftCard tempCard = new GiftCard();
-            tempCard.setName("Card Test #" + i);
-            tempCard.setBalance(new BigDecimal(25.00));
-            mGiftCardHashMap.put(tempCard.getId(), tempCard);
-        }*/
     }
 
-    public void addCard(GiftCard card){
-        //mGiftCardHashMap.put(card.getId(), card);
-
-        //---
+    /**
+     * Adds a card to the database.
+     *
+     * @param card the {@link GiftCard} to be added
+     */
+    public void addCard(GiftCard card) {
         ContentValues values = getContentValues(card);
         mDatabase.insert(GiftCardTable.NAME, null, values);
     }
 
+    /**
+     * Returns the current list of {@link GiftCard}.
+     *
+     * @return {@link List<GiftCard>}
+     */
     public List<GiftCard> getGiftCardList() {
-
+        // create a new list
         List<GiftCard> cards = new ArrayList<>();
-
+        // query database for all rows
         GiftCardCursorWrapper cursor = queryGiftCards(null, null);
 
+        // try to move through rows and add cards to list
         try {
             cursor.moveToFirst();
-            while (!cursor.isAfterLast()){
+            while (!cursor.isAfterLast()) {
                 cards.add(cursor.getGiftCard());
                 cursor.moveToNext();
             }
@@ -67,18 +84,24 @@ public class GiftCardLedger {
         }
 
         return cards;
-
-        //return new ArrayList<>(mGiftCardHashMap.values());
     }
 
+    /**
+     * Query database for a {@link GiftCard} entry
+     *
+     * @param id the id to be queried
+     * @return the {@link GiftCard} if found, otherwise null
+     */
     public GiftCard getGiftCard(UUID id) {
+        // query database for a gift card that matches the given UUID
         GiftCardCursorWrapper cursor = queryGiftCards(
                 GiftCardTable.Cols.UUID + " = ?",
-                new String[] { id.toString() }
+                new String[]{id.toString()}
         );
 
+        // return the gift card if found, otherwise null
         try {
-            if (cursor.getCount() == 0){
+            if (cursor.getCount() == 0) {
                 return null;
             }
             cursor.moveToFirst();
@@ -88,20 +111,36 @@ public class GiftCardLedger {
         }
     }
 
-    public void removeGiftCard(UUID id){
-        mDatabase.delete(GiftCardTable.NAME, GiftCardTable.Cols.UUID + " = ?", new String[] { id.toString() });
+    /**
+     * Removes a card from the database.
+     *
+     * @param id the UUID of the card to be removed.
+     */
+    public void removeGiftCard(UUID id) {
+        mDatabase.delete(GiftCardTable.NAME, GiftCardTable.Cols.UUID + " = ?", new String[]{id.toString()});
     }
 
-    public void updateGiftCard(GiftCard card){
+    /**
+     * Update a {@link GiftCard} in the database.
+     *
+     * @param card the {@link GiftCard} to be updated.
+     */
+    public void updateGiftCard(GiftCard card) {
         String uuidString = card.getId().toString();
         ContentValues values = getContentValues(card);
 
         mDatabase.update(GiftCardTable.NAME, values,
                 GiftCardTable.Cols.UUID + " = ?",
-                new String[] { uuidString });
+                new String[]{uuidString});
     }
 
-    private static ContentValues getContentValues(GiftCard card){
+    /**
+     * Returns a {@link ContentValues} object that can be used to the column of any {@link GiftCard}.
+     *
+     * @param card the {@link GiftCard} to be converted to a ContentValue object
+     * @return a {@link ContentValues} object that can be used with the database.
+     */
+    private static ContentValues getContentValues(GiftCard card) {
         ContentValues values = new ContentValues();
         values.put(GiftCardTable.Cols.UUID, card.getId().toString());
         values.put(GiftCardTable.Cols.NAME, card.getName());
@@ -111,7 +150,14 @@ public class GiftCardLedger {
         return values;
     }
 
-    private GiftCardCursorWrapper queryGiftCards(String whereClause, String[] whereArgs){
+    /**
+     * Returns a {@link GiftCardCursorWrapper} that can be used to easily parse data returned from the database.
+     *
+     * @param whereClause the sqlite WHERE clause
+     * @param whereArgs   the sqlite WHERE arguments
+     * @return a {@link GiftCardCursorWrapper}
+     */
+    private GiftCardCursorWrapper queryGiftCards(String whereClause, String[] whereArgs) {
         Cursor cursor = mDatabase.query(
                 GiftCardTable.NAME,
                 null, //Select * columns
